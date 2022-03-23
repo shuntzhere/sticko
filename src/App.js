3f2c91f88427ca7cd216006ecbeedbe3df72c1d0
Masoom Kumar <masoom.kr04@gmail.com>
2022-03-23 11:54:12 +0530

    BAsic Routing Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
 import logo from "./logo.png";
+import { Paths } from "./routes/Paths";
 
 function App() {
-  return <div className="App">Selling stickers</div>;
+  return (
+    <div className="App">
+      <h1>Selling stickers</h1>
+      <Paths />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/Paths.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import { Cart, Home, Login, Products, Signup, Wishlist } from "../pages/_pages";
+
+export const Paths = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+};
