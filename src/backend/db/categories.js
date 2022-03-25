c72b5a1352e4d941276391955dd17639afdede68
Masoom Kumar <masoom.kr04@gmail.com>
2022-03-26 02:19:25 +0530

    Fix: Updated colors for categories

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -12,7 +12,7 @@ export const categories = [
     title: "Vulnerable",
     description:
       "A vulnerable species is a species which has been categorized by the International Union for Conservation of Nature that is threatened with extinction unless the circumstances that are threatening its survival and reproduction improve.",
-    color: "amber",
+    color: "darkorange",
   },
   {
     _id: uuid(),
@@ -20,7 +20,7 @@ export const categories = [
     title: "Endangered",
     description:
       "An endangered species is a species that is very likely to become extinct in the near future, either worldwide or in a particular political jurisdiction.",
-    color: "red",
+    color: "darkred",
   },
   {
     _id: uuid(),
