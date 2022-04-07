15bac3dae03f1a8f8747fadfb0daf1ac45839517
Masoom Kumar <masoom.kr04@gmail.com>
2022-04-08 00:04:52 +0530

    Fix: Updated auth pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const fetchUser = () => {
+    if (email && password) {
+      navigate("/products");
+    }
+  };
   document.title = " Log In";
+
   return (
     <section className="form__container">
-      <form id="login-form" className="flex flex-col flex-items-center">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        id="login-form"
+        className="flex flex-col flex-items-center"
+      >
         <h2>Log In</h2>
         <p>with</p>
         <div>
@@ -24,18 +39,34 @@ export const Login = () => {
         <p>or</p>
         <div className="input__form">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
+          <input
+            type="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="input__form">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
+          <input
+            type="password"
+            id="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
           <a className="forget__password" href="#">
             Forgot Password ?
           </a>
         </div>
-        <button className="btn btn-submit" type="submit">
+        <button
+          className="btn btn-submit"
+          type="submit"
+          onClick={() => fetchUser()}
+        >
           Log In
         </button>
       </form>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleIcon from "@mui/icons-material/Google";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Signup = () => {
+  const [msg, setMsg] = useState(false);
+  const [type, setType] = useState("password");
+  const [confirmtype, setConfirmType] = useState("password");
+  const [signup, setSignup] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmpass: "",
+  });
+
+  const navigate = useNavigate();
+
+  const changeHandler = (e) => {
+    const value = e.target.value;
+    setSignup({
+      ...signup,
+      [e.target.name]: value,
+    });
+  };
+
+  const clickHandler = () => {
+    const { email, password, username, confirmpass } = signup;
+    if (email && password && username && confirmpass) {
+      if (password === confirmpass) {
+        navigate("/products");
+      } else {
+        setMsg(true);
+      }
+    }
+  };
+
+  const showPass = () => {
+    if (type === "password") {
+      setType("text");
+    } else {
+      setType("password");
+    }
+  };
+
+  const showConfirmPass = () => {
+    if (confirmtype === "password") {
+      setConfirmType("text");
+    } else {
+      setConfirmType("password");
+    }
+  };
   document.title = "Sign Up";
   return (
     <section className="form__container">
-      <form id="signup-form" className="flex flex-col flex-items-center">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        id="signup-form"
+        className="flex flex-col flex-items-center"
+      >
         <h2>Sign Up</h2>
         <p>with</p>
         <div>
@@ -24,36 +75,61 @@ export const Signup = () => {
         <p>or</p>
         <div className="input__form">
           <label htmlFor="name">Username</label>
-          <input type="email" id="email" placeholder="saviour101" required />
+          <input
+            type="text"
+            id="text"
+            defaultValue={signup.username}
+            onChange={changeHandler}
+            placeholder="saviour101"
+            required
+          />
         </div>
         <div className="input__form">
           <label htmlFor="email">email</label>
           <input
             type="email"
             id="email"
+            defaultValue={signup.email}
+            onChange={changeHandler}
             placeholder="sticko@world.com"
             required
           />
         </div>
         <div className="input__form">
           <label htmlFor="password">Password</label>
+          <span
+            onClick={showPass}
+            style={{ cursor: "pointer", marginLeft: "10rem" }}
+          >
+            show
+          </span>
           <input
-            type="password"
+            type={type}
             placeholder="********"
+            defaultValue={signup.password}
+            onChange={changeHandler}
             id="password"
             required
           />
         </div>
         <div className="input__form">
           <label htmlFor="password">Confirm Password</label>
+          <span
+            onClick={showConfirmPass}
+            style={{ cursor: "pointer", marginLeft: "6rem" }}
+          >
+            show
+          </span>
           <input
-            type="password"
+            type={confirmtype}
             placeholder="********"
+            defaultValue={signup.confirmpass}
+            onChange={changeHandler}
             id="password"
             required
           />
         </div>
-        <button className="btn btn-submit" type="submit">
+        <button onClick={clickHandler} className="btn btn-submit" type="submit">
           Sign Up
         </button>
       </form>
